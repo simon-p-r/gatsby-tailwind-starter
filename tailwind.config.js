46d033c7c8d2886decc17a0b6d4471fad03632c5
simon-p-r <simon.ricaldone@circabs.com>
2022-04-18 20:58:25 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,png}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants: {
+    extend: {
+      textColor: ['visited'],
+    }
+  },
+}
